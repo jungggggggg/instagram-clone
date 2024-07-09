e3e0f68a2d300ca9cafcaf6b64fdbccc6b9ed547
jungggggggg <jungsu0705@naver.com>
2024-07-09 14:24:59 +0900

    Cloudinary Images

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -1,21 +1,45 @@
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, useWindowDimensions } from "react-native";
 import { Ionicons, Feather, AntDesign } from '@expo/vector-icons'
-import posts from '~/assets/data/posts.json'
+
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from 'cloudinary-react-native';
+
+// Import required actions and qualifiers.
+import { thumbnail } from "@cloudinary/url-gen/actions/resize";
+import { byRadius } from "@cloudinary/url-gen/actions/roundCorners";
+import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
+import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
+
+//lib/cloudinary에서 cld함수를 가져옴.(외부 api)
+import { cld } from "~/src/lib/cloudinary";
+
+
 
 //모든 포스트를 같은 형식으로 홈화면에 올려줌 
 export default function  PostListItem({ post }) {
+
+    const { width } = useWindowDimensions();
+
+    const Image = cld.image(post.image);
+    Image.resize(thumbnail().width(width).height(width)) 
+    
+    const avatar = cld.image(post.user.avatar_url);
+    Image.resize(thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face())))
 
     return (
         <View className='bg-white'>
             {/* Header */}
             <View className="p-3 flex-row items-center gap-2">
-                <Image source={{ uri: post.user.image_url }} 
+                <AdvancedImage cldImg={avatar} 
                 className="w-12 aspect-square rounded-full" />
                 <Text className="font-semibold">{post.user.username}</Text>
             </View>
 
-            <Image source={{uri: post.image_url}} 
-            className="w-full aspect-[4/3]"/>
+            {/* content */}
+            <AdvancedImage cldImg={Image} className="w-full aspect-[4/3]"/>
+            {/* assets파일은 주석처리, api사용
+             <Image source={{uri: post.image_url}} 
+            className="w-full aspect-[4/3]"/> */}
 
             {/* Icons */}
             <View className="flex-row gap-3 p-3">
---
+++ src/lib/cloudinary.ts
@@ -0,0 +1,8 @@
+
+import { Cloudinary } from "@cloudinary/url-gen";
+
+export const cld = new Cloudinary({
+    cloud: {
+        cloudName: process.env.EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    },
+});
