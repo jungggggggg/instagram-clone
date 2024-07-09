3f8136e6f90054e19605bdea8ae7789a52b60ee6
jungggggggg <jungsu0705@naver.com>
2024-07-09 12:04:37 +0900

    Responsive Feed

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 //홈페이지
-import { FlatList } from "react-native"
+import { FlatList,  } from "react-native"
 import posts from '~/assets/data/posts.json'
 import PostListItem from '~/src/components/PostListItem';
 
@@ -10,10 +10,11 @@ export default function FeedScreen() {
     return (
         <FlatList 
          data={posts}
-         contentContainerStyle={{ gap: 10 }}
-         /* postlistitem 컴포넌트를 components/PostlistItem에서 받아왔음 */
+         className="items-center"
          renderItem={({ item }) => <PostListItem post={item}/>}
+         contentContainerStyle={{ gap: 10, maxWidth: 512, width: '100%' }}
+         /* postlistitem 컴포넌트를 components/PostlistItem에서 받아왔음 */
          showsVerticalScrollIndicator={false}
         />
     )
-}
+} 
